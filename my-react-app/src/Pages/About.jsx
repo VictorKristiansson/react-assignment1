bc0303ec9a1907c5d9b952751602e35ab8d7219c
VictorKristiansson <Victor.Kristiansson@stud.sundsgarden.se>
2025-04-28 12:48:46 +0200

    react - portfolio
    The project is complete and fully functional. I have linked the different pages using React Router and applied custom styling with CSS. The project serves its purpose.

--- my-react-app/src/Pages/About.jsx
+++ my-react-app/src/Pages/About.jsx
@@ -1,6 +1,13 @@
+import '../components/about.css'; 
+
 function About() {
-    return <div>
-        <h1>About</h1>
+    return <div className='about-page'>
+        <h1>About Me</h1>
+        <p>I’ve just started my journey as a programmer, eager to learn and explore new technologies. 
+            This portfolio highlights the projects I’ve worked on so far, 
+            each one marking a step in my early development. While I’m still in the beginning stages, 
+            I’m excited to keep learning and improving over time.
+        </p>
     </div>
 };
 
--- my-react-app/src/Pages/Home.jsx
+++ my-react-app/src/Pages/Home.jsx
@@ -1,6 +1,12 @@
+import '../components/home.css'; 
+
 function Home() {
-    return <div>
-        <h1>Homepage</h1>
+    return <div className='homepage'>
+        <h1> Welcome!</h1>
+        <p>Here you can explore a collection of projects I have worked on. 
+        Feel free to browse through my work and see how I approach different challenges and solutions. 
+        You can check them out by clicking <a href="/portfolio">here</a>.</p>
+       
     </div>
 };
 
